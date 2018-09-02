c4445ad2df17c29efab866a20c5b55c38efae622
Sugarcandy <1947291437@qq.com>
2018-09-02 19:30:34 +0800

    add layout

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String city;
+
+    @SerializedName("id")
+    public String weather;
+
+    public Update update;
+
+    public class Update
+    {
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
